31e9015b908438d2b21a9256165f0e5e3f1c37a2
liuwei <wei2460@qq.com>
2021-04-21 11:20:27 +0800

    chore: update dist

--- dist/core/stack.d.ts
+++ dist/core/stack.d.ts
@@ -7,14 +7,15 @@ export declare type StackProps = {
     value?: StackItem[];
 };
 export declare class Stack {
-    private value;
+    private items;
     constructor(props?: StackProps);
     push(item: StackItem): number;
     pop(): StackItem | undefined;
     removeAfter(key: string): StackItem[] | undefined;
     clear(): StackItem[];
-    get(key: string): VNode<import("vue").RendererNode, import("vue").RendererElement, {
+    find(key: string): VNode<import("vue").RendererNode, import("vue").RendererElement, {
         [key: string]: any;
     }> | undefined;
+    value(): StackItem[];
 }
 export declare const useStack: () => Stack;
